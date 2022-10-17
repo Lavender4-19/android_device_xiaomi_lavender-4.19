dac92482769ec6385bcaa9b6e461abf41486b5bd
Fbriiiii <87246800+Merma1dmanPrjct@users.noreply.github.com>
2022-10-17 13:57:36 +0700

    Lavender: exTHmUI-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -23,9 +23,9 @@
 
 # Inherit Redmi Note 7 device config
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_lavender.mk
+    $(LOCAL_DIR)/exthm_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_lavender-eng \
-    arrow_lavender-userdebug \
-    arrow_lavender-user
+    exthm_lavender-eng \
+    exthm_lavender-userdebug \
+    exthm_lavender-user
